47bda6fd79c48852a0832984366796c7ddd15fe3
msinkec <sinkec.mihael@gmail.com>
2019-02-15 19:26:41 +0100

    Key detection now gets done in sepperate thread asynchronosly to sample at a fixed rate.

--- rc_controller/rc_controller/gui/main_logic.py
+++ rc_controller/rc_controller/gui/main_logic.py
@@ -1,5 +1,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
+import time
+from threading import Thread
 from .main_gui import Ui_MainWindow
 import controls
 
@@ -22,41 +24,52 @@ class MainWindow():
         # Handle keys for car control to the main window.
         # We attach the method to the gui object to avoid changing
         # the auto-generated file.
-        main_window.keyPressEvent = self.key_press_handler
-        main_window.keyReleaseEvent = self.key_release_handler        
+        main_window.keyPressEvent = self.on_key_press
+        main_window.keyReleaseEvent = self.on_key_release
+        
+        # We open a sepperate thread that is checking for pressed keys and
+        # then preiodically send UDP packets every n milliseconds.
+        key_handler_thread = Thread(target=self.handle_keys)
+        key_handler_thread.start()
 
         main_window.show()
         app.exec_()
 
-    def key_press_handler(self, key_event):
-        # If the key is being held down, this method gets auto-repeated.
-        # This is by design. We want to keep sending UDP packets to the car
-        # because it doesn't keep a state about the keys for now.
-        key = key_event.key()
-        self.pressed_keys.add(key)
+    def on_key_press(self, key_event):
+        if not key_event.isAutoRepeat():
+            key = key_event.key()
+            self.pressed_keys.add(key)
 
-        self.handle_keys()
-
-    def key_release_handler(self, key_event):
+    def on_key_release(self, key_event):
         if not key_event.isAutoRepeat():   
             key = key_event.key() 
             self.pressed_keys.discard(key)
 
     def handle_keys(self):
-        # Translate keys into commands and hand them over to the control pacakge.
-        commands = set()
-        for key in self.pressed_keys:
-            if key == QtCore.Qt.Key_Up or key == QtCore.Qt.Key_W:
-                commands.add('f')   # Forward
-            elif key == QtCore.Qt.Key_Down or key == QtCore.Qt.Key_S:
-                commands.add('b')   # Backwards
-            elif key == QtCore.Qt.Key_Right or key == QtCore.Qt.Key_D:
-                commands.add('r')   # Steer right 
-            elif key == QtCore.Qt.Key_Left or key == QtCore.Qt.Key_A:
-                commands.add('l')   # Steer left 
-            elif key == QtCore.Qt.Key_Shift:
-                commands.add('s')   # Speed mode
-        controls.execute(commands, self.sock, self.car_addr, self.controls_port)
+        while True:
+            # Make sure this time period is shorter than:
+            # (network latency + command duration on the car).
+            # Otherwise the car motors will stutter.
+            time.sleep(0.01)
 
+            # Translate keys into commands and hand them over to the control pacakge.
+            commands = set()
+
+            if len(self.pressed_keys) == 0:
+                continue
+
+            for key in self.pressed_keys:
+                if key == QtCore.Qt.Key_Up or key == QtCore.Qt.Key_W:
+                    commands.add('f')   # Forward
+                elif key == QtCore.Qt.Key_Down or key == QtCore.Qt.Key_S:
+                    commands.add('b')   # Backwards
+                elif key == QtCore.Qt.Key_Right or key == QtCore.Qt.Key_D:
+                    commands.add('r')   # Steer right 
+                elif key == QtCore.Qt.Key_Left or key == QtCore.Qt.Key_A:
+                    commands.add('l')   # Steer left 
+                elif key == QtCore.Qt.Key_Shift:
+                    commands.add('s')   # Speed mode
+            print(commands)
+            controls.execute(commands, self.sock, self.car_addr, self.controls_port)
         
 
